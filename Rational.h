269462eaf709dcc08a521a81fa1009e32fc5cb06
Klivree <68071841+Klivree@users.noreply.github.com>
2020-07-16 21:32:11 -0400

    Update Rational.h

--- Rational.h
+++ Rational.h
@@ -16,7 +16,7 @@ public:
 
 	void setRational(); // Constructor function which asks the user to put numbers
 	void autosetRational(int, int); // Constructor function with already existing numbers
-	void getReducedRational(); // Searching for the GCD to reduce the fraction and reduce the fraction if it is possible
+	void getReducedRational(); // Getting the rests of all divisions between the numerator and denominator until it is 0 to get the reduced fraction
 	std::string toRationalString(); // Output the reduced fraction, if it is an integer, it will output the integer
 
 	void getcommunDenominator(Rational&, Rational&); // multiply the fraction with both denominators to have a commun denominator
